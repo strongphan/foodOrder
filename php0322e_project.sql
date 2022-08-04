c4101b66bff944818d1cbde746cb24b12cc9e3ce
Manh Phan <ptm22022002@gmail.com>
2022-08-04 20:55:09 +0700

    upload file database

---
+++ php0322e_project.sql
@@ -0,0 +1,347 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 04, 2022 at 03:53 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 7.4.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `php0322e_project`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admins`
+--
+
+CREATE TABLE `admins` (
+  `id` int(11) NOT NULL,
+  `adminname` varchar(255) DEFAULT NULL COMMENT 'Tên đăng nhập',
+  `password` varchar(255) DEFAULT NULL COMMENT 'Mật khẩu đăng nhập',
+  `first_name` varchar(255) DEFAULT NULL COMMENT 'Fist name',
+  `last_name` varchar(255) DEFAULT NULL COMMENT 'Last name',
+  `phone` int(11) DEFAULT NULL COMMENT 'SĐT user',
+  `address` varchar(255) DEFAULT NULL COMMENT 'Địa chỉ user',
+  `email` varchar(255) DEFAULT NULL COMMENT 'Email của user',
+  `avatar` varchar(255) DEFAULT NULL COMMENT 'File ảnh đại diện',
+  `jobs` varchar(255) DEFAULT NULL COMMENT 'Nghề nghiệp',
+  `last_login` datetime DEFAULT NULL COMMENT 'Lần đăng nhập gần đây nhất',
+  `facebook` varchar(255) DEFAULT NULL COMMENT 'Link facebook',
+  `status` tinyint(3) DEFAULT 0 COMMENT 'Trạng thái danh mục: 0 - Inactive, 1 - Active',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() COMMENT 'Ngày tạo',
+  `updated_at` datetime DEFAULT NULL COMMENT 'Ngày cập nhật cuối'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `admins`
+--
+
+INSERT INTO `admins` (`id`, `adminname`, `password`, `first_name`, `last_name`, `phone`, `address`, `email`, `avatar`, `jobs`, `last_login`, `facebook`, `status`, `created_at`, `updated_at`) VALUES
+(0, 'manhphan', '$2y$10$8s6B7uY8vGWTxpfBm9gdBeMcF9z1UBhjrdqmbZCbUCBBMa1BCBlMu', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, '2022-07-23 02:30:32', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL COMMENT 'Tên danh mục',
+  `type` tinyint(3) DEFAULT 0 COMMENT 'Loại danh mục: 0 - Product, 1 - News',
+  `avatar` varchar(255) DEFAULT NULL COMMENT 'Tên file ảnh danh mục',
+  `description` text DEFAULT NULL COMMENT 'Mô tả chi tiết cho danh mục',
+  `status` tinyint(3) DEFAULT 0 COMMENT 'Trạng thái danh mục: 0 - Inactive, 1 - Active',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() COMMENT 'Ngày tạo danh mục',
+  `updated_at` datetime DEFAULT NULL COMMENT 'Ngày cập nhật cuối'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `name`, `type`, `avatar`, `description`, `status`, `created_at`, `updated_at`) VALUES
+(1, 'Bugger', 0, '1658544648-burger.jpg', 'Bugger\r\n', 1, '2022-06-23 12:35:02', '2022-07-23 10:15:56'),
+(2, 'Pizza', 0, '1658544700-menu-pizza.jpg', 'Pizza ý\r\n', 1, '2022-06-23 12:35:29', '2022-07-23 10:13:35'),
+(3, 'Momo', 0, '1658544742-menu-momo.jpg', 'Há cảo', 1, '2022-06-23 12:35:44', '2022-07-23 10:34:28');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `news`
+--
+
+CREATE TABLE `news` (
+  `id` int(11) NOT NULL,
+  `category_id` int(11) DEFAULT NULL COMMENT 'Id của danh mục mà tin tức thuộc về, là khóa ngoại liên kết với bảng categories',
+  `name` varchar(255) NOT NULL COMMENT 'Tiêu đề tin tức',
+  `summary` varchar(255) DEFAULT NULL COMMENT 'Mô tả ngắn cho tin tức',
+  `avatar` varchar(255) DEFAULT NULL COMMENT 'Tên file ảnh tin tức',
+  `content` text DEFAULT NULL COMMENT 'Mô tả chi tiết cho sản phẩm',
+  `status` tinyint(3) DEFAULT 0 COMMENT 'Trạng thái danh mục: 0 - Inactive, 1 - Active',
+  `seo_title` varchar(255) DEFAULT NULL COMMENT 'Từ khóa seo cho title',
+  `seo_description` varchar(255) DEFAULT NULL COMMENT 'Từ khóa seo cho phần mô tả',
+  `seo_keywords` varchar(255) DEFAULT NULL COMMENT 'Các từ khóa seo',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() COMMENT 'Ngày tạo',
+  `updated_at` datetime DEFAULT NULL COMMENT 'Ngày cập nhật cuối'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL COMMENT 'Id của user trong trường hợp đã login và đặt hàng, là khóa ngoại liên kết với bảng users',
+  `fullname` varchar(255) DEFAULT NULL COMMENT 'Tên khách hàng',
+  `address` varchar(255) DEFAULT NULL COMMENT 'Địa chỉ khách hàng',
+  `mobile` int(11) DEFAULT NULL COMMENT 'SĐT khách hàng',
+  `email` varchar(255) DEFAULT NULL COMMENT 'Email khách hàng',
+  `note` text DEFAULT NULL COMMENT 'Ghi chú từ khách hàng',
+  `price_total` int(11) DEFAULT NULL COMMENT 'Tổng giá trị đơn hàng',
+  `payment_status` tinyint(2) DEFAULT NULL COMMENT 'Trạng thái đơn hàng: 0 - Chưa thành toán, 1 - Đã thành toán',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() COMMENT 'Ngày tạo đơn',
+  `updated_at` datetime DEFAULT NULL COMMENT 'Ngày cập nhật cuối'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`id`, `user_id`, `fullname`, `address`, `mobile`, `email`, `note`, `price_total`, `payment_status`, `created_at`, `updated_at`) VALUES
+(1, NULL, 'Phan Tiến Mạnh', NULL, NULL, NULL, NULL, 345, NULL, '2022-07-07 13:30:52', NULL),
+(2, NULL, '', NULL, NULL, NULL, NULL, 345, NULL, '2022-07-07 13:50:23', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_details`
+--
+
+CREATE TABLE `order_details` (
+  `order_id` int(11) DEFAULT NULL COMMENT 'Id của order tương ứng, là khóa ngoại liên kết với bảng orders',
+  `product_name` varchar(150) DEFAULT NULL COMMENT 'Tên sp tại thời điểm đặt hàng',
+  `product_price` int(11) DEFAULT NULL COMMENT 'Giá sản phẩm tương ứng tại thời điểm đặt hàng',
+  `quantity` int(11) DEFAULT NULL COMMENT 'Số lượng sản phẩm tương ứng tại thời điểm đặt hàng'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `order_details`
+--
+
+INSERT INTO `order_details` (`order_id`, `product_name`, `product_price`, `quantity`) VALUES
+(2, 'bcd', 345, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL,
+  `category_id` int(11) DEFAULT NULL COMMENT 'Id của danh mục mà sản phẩm thuộc về, là khóa ngoại liên kết với bảng categories',
+  `title` varchar(255) DEFAULT NULL COMMENT 'Tên sản phẩm',
+  `avatar` varchar(255) DEFAULT NULL COMMENT 'Tên file ảnh sản phẩm',
+  `price` int(11) DEFAULT NULL COMMENT 'Giá sản phẩm',
+  `amount` int(11) DEFAULT NULL COMMENT 'Số lượng sản phẩm trong kho',
+  `summary` varchar(255) DEFAULT NULL COMMENT 'Mô tả ngắn cho sản phẩm',
+  `content` text DEFAULT NULL COMMENT 'Mô tả chi tiết cho sản phẩm',
+  `status` tinyint(3) DEFAULT 0 COMMENT 'Trạng thái danh mục: 0 - Inactive, 1 - Active',
+  `seo_title` varchar(255) DEFAULT NULL COMMENT 'Từ khóa seo cho title',
+  `seo_description` varchar(255) DEFAULT NULL COMMENT 'Từ khóa seo cho phần mô tả',
+  `seo_keywords` varchar(255) DEFAULT NULL COMMENT 'Các từ khóa seo',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() COMMENT 'Ngày tạo',
+  `updated_at` datetime DEFAULT NULL COMMENT 'Ngày cập nhật cuối'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `category_id`, `title`, `avatar`, `price`, `amount`, `summary`, `content`, `status`, `seo_title`, `seo_description`, `seo_keywords`, `created_at`, `updated_at`) VALUES
+(1, 2, 'Pizza bò dứa', '1658546664-product-pizza.jpg', 123, 80, 'dsad', 'ádas', 1, 'dsadsad', 'dsadsa', 'dsadas', '2022-06-30 11:37:36', '2022-07-23 10:24:24'),
+(2, 1, 'bugger double chesse', '1658998093-product-menu-momo.jpg', 345, 1, 'siêu nhiều phô mai\r\n', 'a', 1, '', '', 'sadsa', '2022-06-30 11:37:56', '2022-07-28 15:48:13'),
+(3, 1, 'bugger bò', '1658546516-product-menu-burger.jpg', 678, 11, 'siêu ngon, siêu đầy đặn', 'Làm từ thịt bò úc 100%', 1, '', '', '', '2022-06-30 11:38:11', '2022-07-23 10:21:56');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `system_settings`
+--
+
+CREATE TABLE `system_settings` (
+  `id` int(30) NOT NULL,
+  `name` text NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `contact` varchar(20) NOT NULL,
+  `cover_img` text NOT NULL,
+  `about_content` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `system_settings`
+--
+
+INSERT INTO `system_settings` (`id`, `name`, `email`, `contact`, `cover_img`, `about_content`) VALUES
+(1, 'Online Food Ordering System', 'info@sample.com', '+6948 8542 623', '1600654680_photo-1504674900247-0877df9cc836.jpg', '&lt;p style=&quot;text-align: center; background: transparent; position: relative;&quot;&gt;&lt;span style=&quot;font-size:28px;background: transparent; position: relative;&quot;&gt;ABOUT US&lt;/span&gt;&lt;/b&gt;&lt;/span&gt;&lt;/p&gt;&lt;p style=&quot;text-align: center; background: transparent; position: relative;&quot;&gt;&lt;span style=&quot;background: transparent; position: relative; font-size: 14px;&quot;&gt;&lt;span style=&quot;font-size:28px;background: transparent; position: relative;&quot;&gt;&lt;b style=&quot;margin: 0px; padding: 0px; color: rgb(0, 0, 0); font-family: &amp;quot;Open Sans&amp;quot;, Arial, sans-serif; text-align: justify;&quot;&gt;Lorem Ipsum&lt;/b&gt;&lt;span style=&quot;color: rgb(0, 0, 0); font-family: &amp;quot;Open Sans&amp;quot;, Arial, sans-serif; font-weight: 400; text-align: justify;&quot;&gt;&amp;nbsp;is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&amp;#x2019;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.&lt;/span&gt;&lt;br&gt;&lt;/span&gt;&lt;/b&gt;&lt;/span&gt;&lt;/p&gt;&lt;p style=&quot;text-align: center; background: transparent; position: relative;&quot;&gt;&lt;span style=&quot;background: transparent; position: relative; font-size: 14px;&quot;&gt;&lt;span style=&quot;font-size:28px;background: transparent; position: relative;&quot;&gt;&lt;span style=&quot;color: rgb(0, 0, 0); font-family: &amp;quot;Open Sans&amp;quot;, Arial, sans-serif; font-weight: 400; text-align: justify;&quot;&gt;&lt;br&gt;&lt;/span&gt;&lt;/b&gt;&lt;/span&gt;&lt;/p&gt;&lt;p style=&quot;text-align: center; background: transparent; position: relative;&quot;&gt;&lt;span style=&quot;background: transparent; position: relative; font-size: 14px;&quot;&gt;&lt;span style=&quot;font-size:28px;background: transparent; position: relative;&quot;&gt;&lt;h2 style=&quot;font-size:28px;background: transparent; position: relative;&quot;&gt;Where does it come from?&lt;/h2&gt;&lt;p style=&quot;text-align: center; margin-bottom: 15px; padding: 0px; color: rgb(0, 0, 0); font-family: &amp;quot;Open Sans&amp;quot;, Arial, sans-serif; font-weight: 400;&quot;&gt;Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of &quot;de Finibus Bonorum et Malorum&quot; (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, &quot;Lorem ipsum dolor sit amet..&quot;, comes from a line in section 1.10.32.&lt;/p&gt;&lt;/span&gt;&lt;/b&gt;&lt;/span&gt;&lt;/p&gt;');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) DEFAULT NULL COMMENT 'Tên đăng nhập',
+  `password` varchar(255) DEFAULT NULL COMMENT 'Mật khẩu đăng nhập',
+  `first_name` varchar(255) DEFAULT NULL COMMENT 'Fist name',
+  `last_name` varchar(255) DEFAULT NULL COMMENT 'Last name',
+  `phone` int(11) DEFAULT NULL COMMENT 'SĐT user',
+  `address` varchar(255) DEFAULT NULL COMMENT 'Địa chỉ user',
+  `email` varchar(255) DEFAULT NULL COMMENT 'Email của user',
+  `avatar` varchar(255) DEFAULT NULL COMMENT 'File ảnh đại diện',
+  `jobs` varchar(255) DEFAULT NULL COMMENT 'Nghề nghiệp',
+  `last_login` datetime DEFAULT NULL COMMENT 'Lần đăng nhập gần đây nhất',
+  `facebook` varchar(255) DEFAULT NULL COMMENT 'Link facebook',
+  `status` tinyint(3) DEFAULT 0 COMMENT 'Trạng thái danh mục: 0 - Inactive, 1 - Active',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() COMMENT 'Ngày tạo',
+  `updated_at` datetime DEFAULT NULL COMMENT 'Ngày cập nhật cuối'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `first_name`, `last_name`, `phone`, `address`, `email`, `avatar`, `jobs`, `last_login`, `facebook`, `status`, `created_at`, `updated_at`) VALUES
+(3, 'admin1', '$2y$10$yQWKg90GUB2xcbb48cKoR.SO841GsLREa823YBFgXWJtxPDGmyHVG', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, '2022-06-23 11:36:22', NULL),
+(5, 'manhphan', '$2y$10$HQZreNvQ5h7Z5/Bfb/wXHucO2MHApOWlk/Ktylhwui3jecdgkLZQy', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, '2022-07-07 10:17:33', NULL),
+(6, 'phanmanh22', '$2y$10$zQzXN2N7J/bmh5OtSkbYE.eMUOnhaiXslgxHC0or3W6XSrpP4psri', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, '2022-07-07 11:17:05', NULL),
+(7, 'hello', '$2y$10$Ru8qCDUceeWUeB.k6YikFuuLMrldhUD4uCRr8hTpR/lqGPrZPP.L2', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, '2022-07-09 01:45:53', NULL),
+(9, 'toilamanh', '$2y$10$.bxjF5W1Y7lwC3zz53fd8eDlKNFlOCCHAjAlqzSAYiw8Ls.8X7G/m', 'Mạnh', 'Phan Tiến', 975169602, 'dăddafaaffwwfa', 'ptm22022002@gmail.com', '1659016818-user-category-img-1.png', '', NULL, '', 0, '2022-07-26 15:31:33', NULL);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `news`
+--
+ALTER TABLE `news`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `category_id` (`category_id`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `order_details`
+--
+ALTER TABLE `order_details`
+  ADD KEY `order_id` (`order_id`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `category_id` (`category_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `news`
+--
+ALTER TABLE `news`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `news`
+--
+ALTER TABLE `news`
+  ADD CONSTRAINT `news_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`);
+
+--
+-- Constraints for table `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `order_details`
+--
+ALTER TABLE `order_details`
+  ADD CONSTRAINT `order_details_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`);
+
+--
+-- Constraints for table `products`
+--
+ALTER TABLE `products`
+  ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
